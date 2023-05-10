02e990129ef11e813631f4e717885eb97d3ead9d
Evan McKinney <evmckinney9@gmail.com>
2023-05-10 18:44:11 -0400

    fix relative import init files

--- src/clonk/backend_utils/mock_backends/__init__.py
+++ src/clonk/backend_utils/mock_backends/__init__.py
@@ -1,11 +1,1 @@
 from .ibm import *
-from .fake_allToAll import FakeAllToAll
-from .fake_example import FakeExampleV2
-from .fake_hatlab import FakeHatlab
-from .fake_heavyhex import FakeHeavyHex
-from .fake_hexLattice import FakeHexLattice
-from .fake_hypercube import FakeHyperCubeV2
-from .fake_hypercubeSNAIL import FakeHyperCubeSnail
-from . fake_modular import FakeModular
-from .fake_surfaceCode import FakeSurfaceCode
-from .fake_tree import FakeTree
